f8859dca4e8d369195832f8460c9a9bc51cde398
davidjjeong <jaewoong.jeong@duke.edu>
2024-11-08 13:41:03 +0900

    Configured default font settings, designed home page with logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,15 +2,30 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const apercu = localFont({
+  src: [
+    {
+      path: "./fonts/ApercuLight.otf",
+      weight: '100',
+      style: 'normal',
+    },
+    {
+      path: "./fonts/ApercuLightItalic.otf",
+      weight: '100',
+      style: 'italic',
+    },
+    {
+      path: "./fonts/ApercuRegular.otf",
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: "./fonts/ApercuBold.otf",
+      weight: '700',
+      style: 'normal',
+    }
+  ],
+  variable: '--font-apercu',
 });
 
 export const metadata: Metadata = {
@@ -26,7 +41,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${apercu.variable} antialiased`}
       >
         {children}
       </body>
