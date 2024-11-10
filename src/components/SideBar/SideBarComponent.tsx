83417460f0dbf838d0968dd89a0b262790df7061
davidjjeong <jaewoong.jeong@duke.edu>
2024-11-10 22:43:28 +0900

    Edited sidebar nav-link to show effect on hover

--- src/components/SideBar/SideBarComponent.tsx
+++ src/components/SideBar/SideBarComponent.tsx
@@ -4,14 +4,12 @@ import { Squares2X2Icon, PencilSquareIcon } from '@heroicons/react/24/outline';
 const SideBar = () => {
     return(
         <nav className={styles.sidebar}>
-            <ul className="flex flex-col justify-center pl-4 gap-y-7">
-                <li className="flex items-center text-[#FFF] text-[18px] font-bold
-                gap-x-1">
+            <ul className="w-[90%] ml-[5%] flex flex-col justify-center">
+                <li className={`${styles.navLink}`}>
                     <Squares2X2Icon className="size-6" />
                     Dashboard
                 </li>
-                <li className="flex items-center text-[#FFF] text-[18px] font-bold
-                gap-x-1">
+                <li className={`${styles.navLink}`}>
                     <PencilSquareIcon className="size-6" />
                     Journal
                 </li>
