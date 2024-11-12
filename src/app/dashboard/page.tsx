ecc88b03f0265f64981acea153c7a7414b7ccad1
David Jeong <97196509+davidjjeong@users.noreply.github.com>
2024-11-12 13:53:30 +0000

    Fetched response from gemini using useChat provider

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useSession } from "next-auth/react";
-import { useState } from "react";
+import { useChat } from "ai/react";
 import SideBar from "@/components/SideBar/SideBarComponent";
 import styles from "./dashboard.module.css";
 
@@ -8,26 +8,9 @@ export default function Dashboard() {
     const { data:session, status } = useSession();
     const user_first_name = session?.user?.name?.split(" ")[0]
 
-    const [response, setResponse] = useState<string>("Hi there! How can I assist you?");
-    const [value, setValue] = useState<string>("");
-
-    const onChange = (e: React.ChangeEvent<HTMLInputElement>) =>
-        setValue(e.target.value)
-    
-    const handleSubmit = async (event:React.KeyboardEvent) => {
-        if(event.key === "Enter"){
-            await fetch("/api/chat", {
-                method: "POST",
-                headers: {
-                    'content-type': 'application/json',
-                },
-                body: JSON.stringify({userInput: value}),
-            }).then((response) => response.json())
-            .then((result: any) => {
-                setResponse(result?.data?.choices[0]?.text);
-            })
-        }
-    };
+    const {messages, input, handleInputChange, handleSubmit} = useChat({
+        api: "api/chat",
+    });
 
     if(status == "authenticated"){
         return(
@@ -35,14 +18,6 @@ export default function Dashboard() {
                 <SideBar />
                 <div className={styles.dashboardMain}>
                     <h1 className="mt-4 ml-10 text-[56px]">Hello, {user_first_name}! 👋</h1>
-                    <input
-                        className="mt-4 ml-10 border-2"
-                        type="text"
-                        value={value}
-                        onChange={onChange}
-                        onKeyDown={(e) => handleSubmit(e)}
-                    />
-                    <h3>{response}</h3>
                 </div>
             </div>
         );
