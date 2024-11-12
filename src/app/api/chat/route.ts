b25c9779e87af38b2c1e3e4e3679f52718943371
David Jeong <97196509+davidjjeong@users.noreply.github.com>
2024-11-12 13:34:29 +0000

    Edited api/chat route handler using ai-sdk vercel

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,5 +1,19 @@
-import { GoogleGenerativeAI } from "@google/generative-ai";
+import { type CoreMessage, streamText } from "ai";
+import { google } from "@ai-sdk/google";
 
-const apiKey = process.env.GEMINI_API_KEY!;
-const genAI = new GoogleGenerativeAI(apiKey)
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+export const maxDuration = 30;
+
+export async function POST(req: Request){
+    const {messages} : {messages: CoreMessage[]} = await req.json();
+
+    const result = await streamText({
+        model: google("models/gemini-1.5-flash-latest"),
+        system: "You are WeMi, a helpful assistant that \
+        will analyze user input and determine the user's \
+        mood and current mental state. You will give a \
+        personalized advice and suggestion with a heart-warming tone.",
+        messages
+    });
+
+    return result.toAIStreamResponse();
+}
